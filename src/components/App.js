5fac8a9e187786495b387d91adcb6d2b24115056
NF-7 <nejc.furh7@gmail.com>
2024-01-07 13:17:10 +0100

    final component commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,60 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>React App</div>
-    ); 
+function App() {
+  const [widgets, setWidgets] = useState([]);
+  
+  function handleOnDrag(e, widgetType) {
+    e.dataTransfer.setData("widgetType", widgetType)
   }
+
+  function handleOnDrop(e) {
+    const widgetType = e.dataTransfer.getData("widgetType")
+    console.log("widgetType", widgetType)
+    setWidgets([...widgets, widgetType])
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault()
+  }
+  
+    return (
+      <div className="app">
+        <h1>React Drag & Drop Component</h1>
+          <div className="widget-container">
+            <div className="widgets">
+              <div 
+                className="widget"
+                draggable
+                onDragStart={(e) => handleOnDrag(e, "Widget A")}
+                >
+                Widget A
+              </div>
+              <div 
+                className="widget"
+                draggable
+                onDragStart={(e) => handleOnDrag(e, "Widget B")}
+                >
+                Widget B
+              </div>
+              <div 
+                className="widget"
+                draggable
+                onDragStart={(e) => handleOnDrag(e, "Widget C")}
+                >
+                Widget C
+              </div>
+            </div>
+
+            <div className="page" onDrop={handleOnDrop} onDragOver={handleDragOver}>
+              {widgets.map((widget, index) => (
+                <div className="dropped-widget" key={index}>
+                  {widget}
+                </div>
+              ))}
+            </div>
+          </div>
+      </div>
+    ); 
 }
 
 export default App;
